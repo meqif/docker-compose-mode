b8810e979f5fe453aeee80042d4eadde8fb2b64c
Ricardo Martins <ricardo@scarybox.net>
2017-07-25 20:03:09 +0100

    Remove duplicated keywords

--- docker-compose-mode.el
+++ docker-compose-mode.el
@@ -34,19 +34,7 @@
   :prefix "docker-compose-")
 
 (defcustom docker-compose-keywords
-  '("aliases" "args" "bridge" "build" "cache_from" "cap_add" "cap_drop"
-    "cgroup_parent" "command" "configs" "container_name" "context"
-    "credential_spec" "depends_on" "deploy" "devices" "dns" "dns_search"
-    "dockerfile" "domainname" "driver" "driver" "driver_opts" "driver_opts"
-    "enable_ipv6" "entrypoint" "env_file" "environment" "expose" "external"
-    "external" "external_links" "extra_hosts" "healthcheck" "hostname" "image"
-    "internal" "ipam" "ipc" "ipv4_address" "ipv6_address" "isolation" "labels"
-    "labels" "labels" "labels" "labels" "links" "logging" "mac_address" "mode"
-    "network_mode" "networks" "overlay" "pid" "placement" "ports" "privileged"
-    "read_only" "replicas" "resources" "restart" "restart_policy" "secrets"
-    "security_opt" "shm_size" "stdin_open" "stop_grace_period" "stop_signal"
-    "sysctls" "tmpfs" "tty" "ulimits" "update_config" "user" "userns_mode"
-    "volumes" "working_dir")
+  '("aliases" "args" "bridge" "build" "cache_from" "cap_add" "cap_drop" "cgroup_parent" "command" "configs" "container_name" "context" "credential_spec" "depends_on" "deploy" "devices" "dns" "dns_search" "dockerfile" "domainname" "driver" "driver_opts" "enable_ipv6" "entrypoint" "env_file" "environment" "expose" "external" "external_links" "extra_hosts" "healthcheck" "hostname" "image" "internal" "ipam" "ipc" "ipv4_address" "ipv6_address" "isolation" "labels" "links" "logging" "mac_address" "mode" "network_mode" "networks" "overlay" "pid" "placement" "ports" "privileged" "read_only" "replicas" "resources" "restart" "restart_policy" "secrets" "security_opt" "shm_size" "stdin_open" "stop_grace_period" "stop_signal" "sysctls" "tmpfs" "tty" "ulimits" "update_config" "user" "userns_mode" "volumes" "working_dir")
   "List of docker-compose keywords."
   :type '(repeat string)
   :group 'docker-compose)
