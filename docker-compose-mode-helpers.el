871285221fc712bf49ced592e11a2faad43f48fe
Ricardo Martins <ricardo@scarybox.net>
2017-07-30 16:25:31 +0100

    Move function helper test to its own file
    Additionally, only run it when the require schema file is present.

--- docker-compose-mode-helpers.el
+++ docker-compose-mode-helpers.el
@@ -76,13 +76,5 @@
              (sort (docker-compose-mode--extract-keywords-from-schema-file it) #'string<)))
      (f-glob "config_schema_*.json" path)))
 
-(describe "Function: `docker-compose-mode--extract-keywords-from-schema-tree'"
-          (describe "given the full docker-compose v1 schema"
-                    (let ((schema-v1-path "config_schema_v1.json"))
-                      (it "returns a list with all the service keys"
-                          (let ((json (json-read-file schema-v1-path))
-                                (expected-keys '(build cap_add cap_drop cgroup_parent command container_name cpu_quota cpu_shares cpuset devices dns dns_search dockerfile domainname entrypoint env_file environment expose extends external_links extra_hosts file hard hostname image ipc labels links log_driver log_opt mac_address mem_limit mem_swappiness memswap_limit net pid ports privileged read_only restart security_opt service shm_size soft stdin_open stop_signal tty ulimits user volume_driver volumes volumes_from working_dir)))
-                            (expect (sort (docker-compose-mode--extract-keywords-from-schema-tree json) #'string<) :to-equal expected-keys))))))
-
 (provide 'docker-compose-mode-helpers)
 ;;; docker-compose-mode-helpers.el ends here
