b3766f7355ac404197fd13b4b5994ec788e67a56
Ricardo Martins <ricardo@scarybox.net>
2017-07-30 23:19:04 +0100

    Update README and Commentary section

--- docker-compose-mode.el
+++ docker-compose-mode.el
@@ -22,6 +22,16 @@
 
 ;;; Commentary:
 
+;; Major mode for editing docker-compose files, providing completion of
+;; docker-compose keys through completion-at-point-functions.
+;;
+;; The completions can be used with the completion system shipped with vanilla
+;; Emacs, and 3rd-party frontends like company-mode, autocomplete, and
+;; ido-at-point.
+;;
+;; By default, the keyword completion function detects the docker-compose
+;; version of the current buffer and suggests the appropriate keywords.
+;;
 ;; See the README for more details.
 
 ;;; Code:
